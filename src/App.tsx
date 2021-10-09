e4c75d874b46c190e3a093bd4fbd1d9676b7d5f3
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-10-09 17:07:10 +0900

    use fmt & fix

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const MyComponent = (): JSX.Element => {
       lng: item.location[0],
       lat: item.location[1],
     };
-    const element = <Marker key={locate.lat+locate.lng} position={locate} />;
+    const element = <Marker key={locate.lat + locate.lng} position={locate} />;
     /*keyの中身は要相談*/
     elements.push(element);
   }
