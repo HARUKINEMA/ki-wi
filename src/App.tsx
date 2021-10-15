3237a62b09b01307439bed0f9d180cb4a7ab3a40
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-10-15 13:58:05 +0900

    マップが中央に大きく表示されるように改善

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Map from "./Map";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 const App = (): JSX.Element => {
   return (
     <div>
-      <Map />
+      <Container>
+        <Row>
+          <Col md={12}>
+            <Map />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
