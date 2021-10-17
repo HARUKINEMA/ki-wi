b33413fc212c6f998758be12c2771be9ebaf877d
根間　春樹 <e195727@ie.u-ryukyu.ac.jp>
2021-10-17 21:50:39 +0900

    stateに苦戦中

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,22 +1,26 @@
 import * as dotenv from "dotenv";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import React from "react";
+import React, { useState } from "react";
 import * as data from "./drinking_machine.json";
-
-const Map = (): JSX.Element => {
+import { Interface } from "readline";
+export interface MapProps{
+  lngprop:number;
+  latprop:number;
+}
+const Map = (props:MapProps): JSX.Element => {
+  const latstate = useState<number>(props.latprop);
+  const lngstate = useState<number>(props.lngprop);
   dotenv.config();
   const containerStyle = {
     width: "400px",
     height: "400px",
   };
-
   const center = {
-    lng: 127.76666539719781,
-    lat: 26.25334632814227,
+    lng: props.lngprop as number,
+    lat: props.latprop as number,
   };
-
+  
   const markerJsx: JSX.Element[] = [];
-
   for (const machine of data.machines) {
     const location = {
       lng: machine.location[0],
@@ -28,7 +32,10 @@ const Map = (): JSX.Element => {
     /*keyの中身は要相談*/
     markerJsx.push(element);
   }
-
+  const changecenter = (): void => {
+    props.latprop=0;
+    props.latprop=0;
+  };
   const api = process.env.REACT_APP_GOOGLE_API_KEY as string;
   return (
     <LoadScript googleMapsApiKey={api}>
@@ -39,5 +46,4 @@ const Map = (): JSX.Element => {
     </LoadScript>
   );
 };
-
 export default Map;
