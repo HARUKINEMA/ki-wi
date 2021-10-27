01ba1dee46210c0c0aae9cdf9f12dde21318ed4c
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-10-27 14:41:31 +0900

    radio button の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Map, { Location } from "./Map";
-import CheckBox, { Building, CheckBoxProps } from "./CheckBox";
+import { Building } from "./CheckBox";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Container, Row } from "react-bootstrap";
 import { Marker } from "@react-google-maps/api";
 import * as data from "./drinking_machine.json";
+import { AreaContainer, AreaContainerProps } from "./AreaContainer";
+import { Col, Container, Row } from "react-bootstrap";
 
 function SetCenter(type: Building): Location {
   let center: Location = {
@@ -21,6 +22,11 @@ function SetCenter(type: Building): Location {
     center = {
       lat: 26.25334632814227,
       lng: 127.76666539719781,
+    };
+  } else if (type == Building.ALL) {
+    center = {
+      lat: 26.247959375749655,
+      lng: 127.76733423620408,
     };
   }
   return center;
@@ -44,18 +50,19 @@ function SelectMarkers(type: Building): JSX.Element[] {
       markersJsx.push(
         <Marker key={location.lat + location.lng} position={location} />
       );
+    } else if (type == Building.ALL) {
+      markersJsx.push(
+        <Marker key={location.lat + location.lng} position={location} />
+      );
     }
   }
   return markersJsx;
 }
 
 const App = (): JSX.Element => {
-  const checkBoxChanged = (isChecked: boolean, type: Building) => {
-    if (!isChecked) {
-      return;
-    }
-    setCenterState(SetCenter(type));
-    setMarkersJsxState(SelectMarkers(type));
+  const onChange = (area: Building) => {
+    setCenterState(SetCenter(area));
+    setMarkersJsxState(SelectMarkers(area));
   };
 
   const markersJsx: JSX.Element[] = [];
@@ -79,34 +86,26 @@ const App = (): JSX.Element => {
   const [markersJsxState, setMarkersJsxState] =
     useState<JSX.Element[]>(markersJsx);
 
-  const checkBoxProps: CheckBoxProps[] = [
-    {
-      label: "工学部",
-      building: Building.FACTORY_OF_ENGINEERING,
-      isChecked: false,
-      toggleCheckBox: checkBoxChanged,
-    },
-    {
-      label: "共通教育",
-      building: Building.COMMON_EDUCATIONAL,
-      isChecked: false,
-      toggleCheckBox: checkBoxChanged,
-    },
-  ];
-
-  const checkBoxJsx: JSX.Element[] = checkBoxProps.map(
-    (checkBox: CheckBoxProps, idx: number) => {
-      return (
-        <CheckBox
-          label={checkBox.label}
-          building={checkBox.building}
-          isChecked={checkBox.isChecked}
-          toggleCheckBox={checkBox.toggleCheckBox}
-          key={idx}
-        />
-      );
-    }
-  );
+  const checkBoxProps: AreaContainerProps = {
+    areaRadioButtons: [
+      {
+        label: "全ての場所",
+        area: Building.ALL,
+        isChecked: true,
+      },
+      {
+        label: "工学部周辺",
+        area: Building.FACTORY_OF_ENGINEERING,
+        isChecked: false,
+      },
+      {
+        label: "共通教育棟周辺",
+        area: Building.COMMON_EDUCATIONAL,
+        isChecked: false,
+      },
+    ],
+    onChangeRadioButton: onChange,
+  };
 
   return (
     <div>
@@ -114,7 +113,16 @@ const App = (): JSX.Element => {
         <Row>
           <Col md={12}>
             <Map center={centerState} markers={markersJsxState} />
-            {checkBoxJsx}
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <AreaContainer
+              areaRadioButtons={checkBoxProps.areaRadioButtons}
+              onChangeRadioButton={(area: Building) =>
+                checkBoxProps.onChangeRadioButton(area)
+              }
+            />
           </Col>
         </Row>
       </Container>
