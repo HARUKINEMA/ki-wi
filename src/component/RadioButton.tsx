5849a4f9d756ccc23e336b12079b099cddf15ad5
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-11-19 21:51:56 +0900

    自動販売機申請ボタンの追加

--- src/component/RadioButton.tsx
+++ src/component/RadioButton.tsx
@@ -7,14 +7,26 @@ export interface RadioButtonProps {
 }
 
 export const RadioButton = (props: RadioButtonProps): JSX.Element => {
+  const inputStyle = {
+    marginRight: "4px",
+    width: "17px",
+    height: "17px",
+  };
+
+  const labelStyle = {
+    marginTop: "2px",
+  };
+
   return (
     <div>
       <input
         type={"radio"}
         checked={props.isChecked}
         onChange={() => props.onChanged()}
+        style={inputStyle}
       />
-      <label> {props.label} </label>
+
+      <label style={labelStyle}> {props.label} </label>
     </div>
   );
 };
